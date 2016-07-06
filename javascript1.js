4063f10ce3d484a78c291dc409d43f364d99c9f3
zac hackett <michael.z.hackett@gmail.com>
2016-07-05 20:49:38 -0400

    initial commit

---
+++ javascript1.js
@@ -0,0 +1,28 @@
+console.log("hey wkend fun");
+
+alert("Holiday TurnUP BRO!")
+
+function logtohtml() {
+	document.getElementById("happy").innerHTML = "funtimes";
+}
+
+var x = 99
+console.log(x + 66)
+
+var colors = ["crimson", "skyblue", "lightgreen"]
+
+console. log(colors[0]);
+console. log(colors[2]);
+
+var info = {
+	firstName: "Michael",
+	lastName: "Hackett",
+	birthday: "Sep 26",
+	interest: ["sports", "movies"],
+	hobbies: ["traveling"]
+	}
+
+	console.log(info.firstname);
+	console.log(info.interest);
+	console.log(info.birthday);
+	console.log(info.hobbies);
